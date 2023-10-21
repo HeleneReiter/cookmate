e940dc014ff274355432041b560747400579f07f
s2110238062 <s2110238062@fhooe.at>
2023-10-21 17:26:43 +0200

    segments

--- src/pages/Schneiden.tsx
+++ src/pages/Schneiden.tsx
@@ -1,23 +1,88 @@
-import React from 'react';
-import { IonHeader, IonIcon, IonLabel, IonSegment, IonSegmentButton, IonToolbar } from '@ionic/react';
+import React, { useState } from 'react';
+import {
+  IonCard,
+  IonCardContent,
+  IonChip,
+  IonHeader,
+  IonSegment,
+  IonSegmentButton,
+  IonLabel,
+  IonPage,
+  IonToolbar
+} from '@ionic/react';
 
 function Schneiden() {
-    return (
-        <IonSegment scrollable={true} value="One">
+  const [currentSegment, setCurrentSegment] = useState("One"); // Initialer Segmentwert
 
-          <IonSegmentButton value="One">
-            <IonHeader>One</IonHeader>
-          </IonSegmentButton>
+  const handleSegmentChange = (event: CustomEvent) => {
+    const newSegmentValue = event.detail.value as string; // Typumwandlung zu string
+    setCurrentSegment(newSegmentValue);
+  };
 
-          <IonSegmentButton value="Two">
-          <IonHeader>Two</IonHeader>
-          </IonSegmentButton>
-
-          <IonSegmentButton value="Three">
-          <IonHeader>Two</IonHeader>
-          </IonSegmentButton>
-          
-        </IonSegment>
-      );
+  // Funktion zum Rendern des Inhalts der IonCard basierend auf dem aktuellen Segmentwert
+  const renderCardContent = () => {
+    switch (currentSegment) {
+      case "One":
+        return (
+          <>
+            <h2>Card Titel - One</h2>
+            <p>Card Text - One</p>
+            <IonChip>
+              <IonLabel>Chip Label - One</IonLabel>
+            </IonChip>
+          </>
+        );
+      case "Two":
+        return (
+          <>
+            <h2>Card Titel - Two</h2>
+            <p>Card Text - Two</p>
+            <IonChip>
+              <IonLabel>Chip Label - Two</IonLabel>
+            </IonChip>
+          </>
+        );
+      case "Three":
+        return (
+          <>
+            <h2>Card Titel - Three</h2>
+            <p>Card Text - Three</p>
+            <IonChip>
+              <IonLabel>Chip Label - Three</IonLabel>
+            </IonChip>
+          </>
+        );
+      default:
+        return null;
     }
+  };
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+         <IonSegment scrollable={true} value={currentSegment} onIonChange={handleSegmentChange}>
+            <IonSegmentButton value="One">
+              One
+            </IonSegmentButton>
+            <IonSegmentButton value="Two">
+              Two
+            </IonSegmentButton>
+            <IonSegmentButton value="Three">
+              Three
+            </IonSegmentButton>
+          </IonSegment>
+        </IonToolbar>
+      </IonHeader>
+      
+      {/* Hier wird der Inhalt der IonCard gerendert basierend auf dem aktuellen Segmentwert */}
+      <IonCard>
+        <IonCardContent>
+          {renderCardContent()}
+        </IonCardContent>
+      </IonCard>
+    </IonPage>
+  );
+}
+
 export default Schneiden;
