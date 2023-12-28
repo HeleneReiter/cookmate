4f36222cc3af3b02e4f3b856bf646326d2e00635
Laceels <s2110238019@fhooe.at>
2023-12-28 23:43:33 +0100

    little corrections

--- src/pages/Start.tsx
+++ src/pages/Start.tsx
@@ -7,32 +7,36 @@ const Start: React.FC = () => {
   const handleStartClick = () => {
     window.location.href = "/loading";
   };
+
   return (
     <IonPage>
-    <IonContent style={{ overflow: 'hidden' }}>
-      <div className="start">
-        <div className="overlap-group-wrapper">
-          <div className="overlap-group">
-            <img
-              className="logo-home"
-              src="/assets/Elemente/Logo_ohnebackground.png"
-            />
-            
-            <img
-              className="chefs-touch"
-              src="/assets/Elemente/ChefsTouchYourKitchen.svg"
-            />
+      <IonContent style={{ overflow: 'hidden' }}>
+        <div className="start">
+          <div className="overlap-group-wrapper">
+            <div className="overlap-group">
+              {/* Add the overlay element */}
+              <div className="overlay"></div>
 
-            <img
-              onClick={handleStartClick}
-              className="jetzt-starten"
-              alt="Jetzt starten"
-              src="/assets/Elemente/JetztStarten.png"
-            />
+              <img
+                className="logo-home"
+                src="/assets/Elemente/Logo_ohnebackground.png"
+              />
+
+              <img
+                className="chefs-touch"
+                src="/assets/Elemente/ChefsTouchYourKitchen.svg"
+              />
+
+              <img
+                onClick={handleStartClick}
+                className="jetzt-starten"
+                alt="Jetzt starten"
+                src="/assets/Elemente/JetztStarten.png"
+              />
+            </div>
           </div>
         </div>
-      </div>
-    </IonContent>
+      </IonContent>
     </IonPage>
   );
 };
