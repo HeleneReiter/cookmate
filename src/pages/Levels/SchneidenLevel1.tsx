db37954232f891f9030d99079cd84dadfcb1c417
s2110238062 <s2110238062@fhooe.at>
2024-01-15 23:28:06 +0100

    neue RecipeVorschau (schneidenlvl1)

--- src/pages/Levels/SchneidenLevel1.tsx
+++ src/pages/Levels/SchneidenLevel1.tsx
@@ -1,46 +1,145 @@
-import React from 'react';
+import React, { useState } from "react";
 import {
+  IonButton,
+  IonCard,
+  IonCardContent,
+  IonChip,
   IonContent,
+  IonLabel,
   IonPage,
-} from '@ionic/react';
+} from "@ionic/react";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/swiper-bundle.css';
 
-import RecipeCard from '../eigeneKomponenten/RecipeCard'; 
+interface SlideData {
+  title: string;
+  description: string;
+  label: string;
+  classname: string;
+  recipeCategoryLabel: string;
+  image: string;
+}
+import Rezept from '../Rezeptseiten/Rezepte/SchneidenRezepte1.json';
 
 const SchneidenLevel1: React.FC = () => {
 
+
+  const data: SlideData[] = Array.from({ length: 3 }, (_, index) => ({
+    title: Rezept.titles[index],
+    description: Rezept.descriptions[index],
+    label: Rezept.labels[index],
+    classname: Rezept.classnames[index],
+    recipeCategoryLabel: Rezept.recipeCategoryLabels[index],
+    image: Rezept.images[index],
+  }));
+
+  const [swiper, setSwiper] = useState<any>(null);
+
+  const goNext = () => {
+    if (swiper) {
+      swiper.slideNext();
+    }
+  }
+
+  const goPrev = () => {
+    if (swiper) {
+      swiper.slidePrev();
+    }
+  };
+
+  const handleCloseClick = () => {
+    window.location.href = "/Home";
+
+  };
+
+
+  let index = 0;
+
+  const handleSlideChange = () => {
+    const currentSlideIndex = swiper.realIndex;
+    index = currentSlideIndex;
+  }
+
+
+  const renderImage = () => {
+    if (index === 0) {
+      return Rezept.images[0];
+
+    } else if (index === 1) {
+      return Rezept.images[1];
+
+    } else if (index === 2) {
+      return Rezept.images[2];
+
+    }
+  }
+
+
   return (
-    <IonPage>
-      <IonContent>
-        <RecipeCard
-          titleOne="Rezept1"
-          descriptionOne="Chinesisch gebratene Nudeln"
-          labelOne='Fleisch'
-          classnameOne='labelMeat'
-          recipeCategoryLabelOne="Hauptspeise"
-          recipeCategoryColorOne="default"
-          imgOne="/assets/Bildrecherche/schneiden_lvl1_gebratenenudeln_.JPG"
+    <IonPage
+      style={{
+        background: `linear-gradient(to bottom, rgba(4,13,16,0.0), rgba(0,0,0,1)), url(${renderImage()})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center'
+      }}
+    >
+      <IonContent scrollX >
+        <Swiper
+          onSwiper={setSwiper}
+          spaceBetween={100}
+          slidesPerView={1}
+          scrollbar={{ draggable: false }}
+          onSlideChange={handleSlideChange}
+        >
 
-          titleTwo="Rezept2"
-          descriptionTwo="Avocadotomatensalat"
-          labelTwo="Vegan"
-          classnameTwo="labelVegan"
-          recipeCategoryLabelTwo="Hauptspeise"
-          recipeCategoryColorTwo="default"
-          imgTwo="/assets/Bildrecherche/schneiden_lvl1_avocadotomatensalat.JPG"
+      <img className="logo" src="/assets/Elemente/Logo.png" alt="Logo" />
+      <img
+        onClick={handleCloseClick}
+        className="close_dark"
+        alt="schließen"
+        src="/assets/Elemente/Close.png" style={{ position: 'absolute', right: '40px', top: '90px' }}
+      />
 
-          titleThree="Rezept3"
-          descriptionThree="Asiatisches Gemuese"
-          labelThree="Vegan"
-          classnameThree="labelVegan"
-          recipeCategoryLabelThree="Hauptspeise"
-          recipeCategoryColorThree="default"
-          imgThree="/assets/Bildrecherche/schneiden_lvl1_asiatischesgemüse.JPG"
+          {data.map((slide, index) => (
+            <SwiperSlide key={`slide_${index}`} >
+              <IonCardContent >
 
-          id="1_"
-        />
+
+                <>
+                  <IonCard className="custom-ion-card">
+                    <div className="box"></div>
+                    <p>{slide.description}</p>
+                    <div className="info"><h2>Gewürze {slide.title}</h2></div> {/*besser korrigieren*/}
+                  </IonCard>
+                  <div className="labelbox">
+                    <IonChip className={slide.classname}>
+                      <IonLabel>{slide.label}</IonLabel>
+                    </IonChip>
+                    <IonChip className="labelCategory">
+                      <IonLabel>{slide.recipeCategoryLabel}</IonLabel>
+                    </IonChip>
+                  </div>
+                  <div className="circleOne"></div>
+                </>
+
+
+
+                {/* <IonButton onClick={goPrev}>&lt;</IonButton>
+
+                <IonButton onClick={goNext}>&gt;</IonButton> */}
+
+
+              </IonCardContent>
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </IonContent>
+
+      <IonButton className="zumRezept" fill="clear" routerLink="/Rezept1_One" >
+        <p>Zum Rezept &gt;</p>
+      </IonButton>
+
     </IonPage>
   );
 };
-
 export default SchneidenLevel1;
---
+++ src/pages/Levels/SchneidenLevel1Save.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import {
+  IonContent,
+  IonPage,
+} from '@ionic/react';
+
+import RecipeCard from '../eigeneKomponenten/RecipeCard'; 
+
+const SchneidenLevel1: React.FC = () => {
+
+  return (
+    <IonPage>
+      <IonContent>
+        <RecipeCard
+          titleOne="Rezept1"
+          descriptionOne="Chinesisch gebratene Nudeln"
+          labelOne='Fleisch'
+          classnameOne='labelMeat'
+          recipeCategoryLabelOne="Hauptspeise"
+          recipeCategoryColorOne="default"
+          imgOne="/assets/Bildrecherche/schneiden_lvl1_gebratenenudeln_.JPG"
+
+          titleTwo="Rezept2"
+          descriptionTwo="Avocadotomatensalat"
+          labelTwo="Vegan"
+          classnameTwo="labelVegan"
+          recipeCategoryLabelTwo="Hauptspeise"
+          recipeCategoryColorTwo="default"
+          imgTwo="/assets/Bildrecherche/schneiden_lvl1_avocadotomatensalat.JPG"
+
+          titleThree="Rezept3"
+          descriptionThree="Asiatisches Gemuese"
+          labelThree="Vegan"
+          classnameThree="labelVegan"
+          recipeCategoryLabelThree="Hauptspeise"
+          recipeCategoryColorThree="default"
+          imgThree="/assets/Bildrecherche/schneiden_lvl1_asiatischesgemüse.JPG"
+
+          id="1_"
+        />
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default SchneidenLevel1;
