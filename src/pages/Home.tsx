5a1ab3f27a58523f77dd775eb83fe35a0337f11d
Laceels <s2110238019@fhooe.at>
2024-01-15 02:22:48 +0100

    completed home.css

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -18,13 +18,22 @@ const Home: React.FC = () => {
       categories: ["Schneiden", "Marinieren", "Teig", "Gewurze"],
     },
   ];
-  const categories = [
+  const schneiden = [
     { name: "Schneiden", levels: [1, 2, 3] },
+  ];
+  const marinieren = [
     { name: "Marinieren", levels: [1, 2, 3] },
+  ];
+  const backen = [
     { name: "Backen", levels: [1, 2, 3] },
+  ];
+  const teig = [
     { name: "Teig", levels: [1, 2, 3] },
+  ];
+  const gewurze = [
     { name: "Gewurze", levels: [1, 2, 3] },
   ];
+
   const bonus = [
     { name: "Anrichten", levels: [1, 2, 3] },
 
@@ -60,10 +69,82 @@ const Home: React.FC = () => {
             </IonCardContent>
           </IonCard>
         ))}
-        {categories.map((category, index) => (
+        {schneiden.map((category, index) => (
           <IonCard key={index} className="category-card">
             <IonCardContent>
-              <h4>Kapitel</h4>
+              <h4 style={{ color: '#FF5D52' }}>Kapitel 1</h4>
+              <h2 className="homeh2">{category.name}</h2>
+              {category.levels.map((level, levelIndex) => (
+                <IonButton
+                  routerLink={`/${category.name.toLowerCase()}/level${level}`}
+                  className="levelButton"
+                  key={levelIndex}
+                  fill="clear"
+                >
+                  LV{level}
+                </IonButton>
+              ))}
+            </IonCardContent>
+          </IonCard>
+        ))}
+        {marinieren.map((category, index) => (
+          <IonCard key={index} className="category-card-dark">
+            <IonCardContent>
+            <h4 style={{ color: '#7EBDC2' }}>Kapitel 2</h4>
+              <h2 className="homeh2-dark">{category.name}</h2>
+              {category.levels.map((level, levelIndex) => (
+                <IonButton
+                  routerLink={`/${category.name.toLowerCase()}/level${level}`}
+                  className="levelButton-dark-marinieren"
+                  key={levelIndex}
+                  fill="clear"
+                >
+                  LV{level}
+                </IonButton>
+              ))}
+            </IonCardContent>
+          </IonCard>
+        ))}
+        {backen.map((category, index) => (
+          <IonCard key={index} className="category-card">
+            <IonCardContent>
+              <h4 style={{ color: '#D50032' }}>Kapitel 3</h4>
+              <h2 className="homeh2">{category.name}</h2>
+              {category.levels.map((level, levelIndex) => (
+                <IonButton
+                  routerLink={`/${category.name.toLowerCase()}/level${level}`}
+                  className="levelButton"
+                  key={levelIndex}
+                  fill="clear"
+                >
+                  LV{level}
+                </IonButton>
+              ))}
+            </IonCardContent>
+          </IonCard>
+        ))}
+        {teig.map((category, index) => (
+          <IonCard key={index} className="category-card-dark">
+            <IonCardContent>
+              <h4 style={{ color: '#F3DFA2' }}>Kapitel 4</h4>
+              <h2 className="homeh2-dark">{category.name}</h2>
+              {category.levels.map((level, levelIndex) => (
+                <IonButton
+                  routerLink={`/${category.name.toLowerCase()}/level${level}`}
+                  className="levelButton-dark-teig"
+                  key={levelIndex}
+                  fill="clear"
+                >
+                  LV{level}
+                </IonButton>
+              ))}
+            </IonCardContent>
+          </IonCard>
+        ))}
+        {gewurze.map((category, index) => (
+          <IonCard key={index} className="category-card">
+            <IonCardContent>
+              <h4 style={{ color: '#FF5D52' }}>Kapitel 5</h4>
               <h2 className="homeh2">{category.name}</h2>
               {category.levels.map((level, levelIndex) => (
                 <IonButton
@@ -81,7 +162,7 @@ const Home: React.FC = () => {
         {bonus.map((category, index) => (
           <IonCard key={index} className="bonus-card">
             <IonCardContent>
-              <h2>Bonus!</h2>
+              <h2>Bonus</h2>
               <h3 className="homeh3">{category.name}</h3>
               {category.levels.map((level, levelIndex) => (
                 <IonButton
@@ -95,6 +176,7 @@ const Home: React.FC = () => {
               ))}
               <h4>Wow,</h4>
               <h4>geschafft!</h4>
+              <br></br>
             </IonCardContent>
           </IonCard>
         ))}
