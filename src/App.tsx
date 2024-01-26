6f012921ea6d39efe6eb7136b24610842ef49531
s2110238062 <s2110238062@fhooe.at>
2024-01-26 15:31:25 +0100

    Rezeptübersichten für andere Levels

--- src/App.tsx
+++ src/App.tsx
@@ -55,12 +55,30 @@ import SchneidenRezept2 from "./pages/Rezeptseiten/Arbeitsschritte/SchneidenReze
 import SchneidenRezept3 from "./pages/Rezeptseiten/Arbeitsschritte/SchneidenRezept3";
 
 import MarinierenLevel1 from "./pages/Levels/MarinierenLevel1";
+import MarinierenLevel2 from "./pages/Levels/MarinierenLevel2";
+import MarinierenLevel3 from "./pages/Levels/MarinierenLevel3";
 import MarinierenRezept1 from "./pages/Rezeptseiten/Arbeitsschritte/MarinierenRezept1";
 
 import Rezept1_One from "./pages/Rezeptseiten/Rezept1_One";
 import Rezept2_One from "./pages/Rezeptseiten/Rezept2_One";
 import Rezept3_One from "./pages/Rezeptseiten/Rezept3_One";
 import Rezept4_One from "./pages/Rezeptseiten/Rezept4_One";
+
+import BackenLevel1 from "./pages/Levels/BackenLevel1";
+import BackenLevel2 from "./pages/Levels/BackenLevel2";
+import BackenLevel3 from "./pages/Levels/BackenLevel3";
+
+import TeigLevel1 from "./pages/Levels/TeigLevel1";
+import TeigLevel2 from "./pages/Levels/TeigLevel2";
+import TeigLevel3 from "./pages/Levels/TeigLevel3";
+
+import GewürzeLevel1 from "./pages/Levels/GewürzeLevel1";
+import GewürzeLevel2 from "./pages/Levels/GewürzeLevel2";
+import GewürzeLevel3 from "./pages/Levels/GewürzeLevel3";
+
+import AnrichtenLevel1 from "./pages/Levels/AnrichtenLevel1";
+import AnrichtenLevel2 from "./pages/Levels/AnrichtenLevel2";
+import AnrichtenLevel3 from "./pages/Levels/AnrichtenLevel3";
 
 import Start from "./pages/Start";
 import Loading from "./pages/Loading";
@@ -91,6 +109,24 @@ const App: React.FC = () => (
           <Route exact path="/schneiden/level2" component={SchneidenLevel2}></Route>
           <Route exact path="/schneiden/level3" component={SchneidenLevel3}></Route>
           <Route exact path="/marinieren/level1" component={MarinierenLevel1}></Route>
+          <Route exact path="/marinieren/level2" component={MarinierenLevel2}></Route>
+          <Route exact path="/marinieren/level3" component={MarinierenLevel3}></Route>
+
+          <Route exact path="/backen/level1" component={BackenLevel1}></Route>
+          <Route exact path="/backen/level2" component={BackenLevel2}></Route>
+          <Route exact path="/backen/level3" component={BackenLevel3}></Route>
+
+          <Route exact path="/teig/level1" component={TeigLevel1}></Route>
+          <Route exact path="/teig/level2" component={TeigLevel2}></Route>
+          <Route exact path="/teig/level3" component={TeigLevel3}></Route>
+
+          <Route exact path="/gewurze/level1" component={GewürzeLevel1}></Route>
+          <Route exact path="/gewurze/level2" component={GewürzeLevel2}></Route>
+          <Route exact path="/gewurze/level3" component={GewürzeLevel3}></Route>
+
+          <Route exact path="/anrichten/level1" component={AnrichtenLevel1}></Route>
+          <Route exact path="/anrichten/level2" component={AnrichtenLevel2}></Route>
+          <Route exact path="/anrichten/level3" component={AnrichtenLevel3}></Route>
          
           <Route exact path="/BasicRezept_Schneiden" component={BasicRezept_Schneiden}></Route>
           <Route exact path="/BasicRezept_Marinieren" component={BasicRezept_Marinieren}></Route>
---
+++ src/pages/Levels/GewürzeLevel2.tsx
@@ -0,0 +1,147 @@
+import React, { useState } from "react";
+import {
+  IonButton,
+  IonCard,
+  IonCardContent,
+  IonChip,
+  IonContent,
+  IonLabel,
+  IonPage,
+} from "@ionic/react";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/swiper-bundle.css';
+import '../CSS/RecipeCard.css';
+
+interface SlideData {
+  title: string;
+  description: string;
+  label: string;
+  classname: string;
+  recipeCategoryLabel: string;
+  image: string;
+}
+import Rezept from '../Rezeptseiten/Rezepte/GewürzeRezepte2.json';
+
+const GewürzeLevel2 : React.FC = () => {
+
+
+  const data: SlideData[] = Array.from({ length: 3 }, (_, index) => ({
+    title: Rezept.titles[index],
+    description: Rezept.descriptions[index],
+    label: Rezept.labels[index],
+    classname: Rezept.classnames[index],
+    recipeCategoryLabel: Rezept.recipeCategoryLabels[index],
+    image: Rezept.images[index],
+  }));
+
+  const circlesFilled = ["circleOneFilled", "circleTwoFilled", "circleThreeFilled"];
+  const circles = ["circleOne", "circleTwo", "circleThree"];
+
+  const [swiper, setSwiper] = useState<any>(null);
+  const [currentImage, setCurrentImage] = useState<string>(data[0].image);
+  const [currentCircleFilled, setCurrentCircleFilled] = useState<string>(circlesFilled[0]);
+  const [currentCircle, setCurrentCircle] = useState<string>(circles[0]);
+
+  const goNext = () => {
+    if (swiper) {
+      swiper.slideNext();
+    }
+  }
+
+  const goPrev = () => {
+    if (swiper) {
+      swiper.slidePrev();
+    }
+  };
+
+  const handleCloseClick = () => {
+    window.location.href = "/Home";
+  };
+
+
+
+  const handleSlideChange = () => {
+    const currentSlideIndex = swiper.realIndex;
+    setCurrentImage(data[currentSlideIndex].image);
+    setCurrentCircleFilled(circlesFilled[currentSlideIndex]);
+    setCurrentCircle(circles[currentSlideIndex]);
+  }
+
+
+  return (
+    <IonPage
+      style={{
+        background: `linear-gradient(to bottom, rgba(4,13,16,0.0), rgba(0,0,0,1)), url(${currentImage})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center'
+      }}
+    >
+      <IonContent
+        scrollX={false}
+        scrollY={false}>
+        <Swiper
+          onSwiper={setSwiper}
+          spaceBetween={100}
+          slidesPerView={1}
+          scrollbar={{ draggable: false }}
+          onSlideChange={handleSlideChange}
+          className="swiper"
+        >
+
+          <img className="logo" src="/assets/Elemente/Logo.png" alt="Logo" />
+          <img
+            onClick={handleCloseClick}
+            className="close_dark"
+            alt="schließen"
+            src="/assets/Elemente/Close.png" style={{ position: 'absolute', right: '40px', top: '90px' }}
+          />
+
+          {data.map((slide, index) => (
+            <SwiperSlide key={`slide_${index}`} className="swiper_slide">
+              <IonCardContent>
+                <>
+
+                  <IonCard className="custom-ion-card">
+                    <div className="box">
+                      <p>{slide.description}</p>
+                      <div className="info"><h2>Marinieren {slide.title}</h2></div>
+                    </div>
+                  </IonCard>
+
+                  <div className="labelbox">
+                    <IonChip className={slide.classname}>
+                      <IonLabel>{slide.label}</IonLabel>
+                    </IonChip>
+                    <IonChip className="labelCategory">
+                      <IonLabel>{slide.recipeCategoryLabel}</IonLabel>
+                    </IonChip>
+                  </div>
+                </>
+                <img
+                  onClick={handleCloseClick}
+                  className="close_dark"
+                  alt="schließen"
+                  src="/assets/Elemente/Close.png" style={{ position: 'absolute', right: '40px', top: '90px', opacity: 0 }}
+                />
+
+              </IonCardContent>
+
+            </SwiperSlide>
+          ))}
+        </Swiper>
+
+      </IonContent>
+
+      <div className="Circles">
+        {circles.map((circle, index) => (
+          <div key={`circle_${index}`} className={currentCircle === circle ? currentCircleFilled : circle} />
+        ))}
+      </div>
+      <IonButton className="zumRezept" fill="clear" routerLink="/Loading" >
+        <p>Zum Rezept &gt;</p>
+      </IonButton>
+
+    </IonPage>
+  );
+};
+export default GewürzeLevel2;
